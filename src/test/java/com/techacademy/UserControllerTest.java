00fe601dc9f40f10d4fac48f947ea0a79349dc3e
tetsuya-takayanagi-09 <tetsuya.takayanagi09@gmail.com>
2026-01-29 12:20:27 +0900

    add unittest

--- src/test/java/com/techacademy/UserControllerTest.java
+++ src/test/java/com/techacademy/UserControllerTest.java
@@ -24,26 +24,17 @@ class UserControllerTest {
 
     @Test
     void testGetList() throws Exception {
-        // リクエストを実行し、結果を取得
         MvcResult result = mockMvc.perform(get("/user/list"))
-            // HTTPステータスが200OKであること
             .andExpect(status().isOk())
-            // Modelにuserlistが含まれていること
             .andExpect(model().attributeExists("userlist"))
-            // Modelにエラーが無いこと
             .andExpect(model().hasNoErrors())
-            // viewの名前が user/list であること
             .andExpect(view().name("user/list"))
             .andReturn();
 
-        // Modelからuserlistを取り出す
-        @SuppressWarnings("unchecked")
         List<User> userlist = (List<User>) result.getModelAndView().getModel().get("userlist");
 
-        // 件数が3件であること
         assertEquals(3, userlist.size());
 
-        // userlistから1件ずつ取り出し、idとnameを検証する
         User user1 = userlist.get(0);
         assertEquals(1, user1.getId());
         assertEquals("キラメキ太郎", user1.getName());
